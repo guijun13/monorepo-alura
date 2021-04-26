90dd74395110f01d608c06ca4e92ac48b55ab3bf
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-26 10:31:49 -0300

    :sparkles: Add typescript types to 'Button' props
    Create description table on Storybook for 'Button' props

--- projects/ui/src/components/Button/index.tsx
+++ projects/ui/src/components/Button/index.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export default function Button({ disabled, children }) {
+interface ButtonProps {
+  /**
+   * Essa prop usa o disabled padrao do HTML
+   */
+  disabled?: boolean;
+  /**
+   * Passa o que renderizar dentro do botão
+   */
+  children: React.ReactNode;
+}
+// Button.propTypes = {
+//   disabled: PropTypes.bool,
+//   children: PropTypes.node.isRequired,
+// }
+
+export default function Button({ disabled, children }: ButtonProps) {
   return <button disabled={disabled}>{children}</button>;
 }
+
+Button.defaultProps = {
+  disabled: false,
+};
