95932d639475c70cef8571062be1324be12bdb26
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-30 00:58:08 -0300

    :hammer: Update @types/react for website and ui

--- projects/website/pages/index.tsx
+++ projects/website/pages/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { UIProvider } from '@guijun13/ui/src/theme/UIProvider';
 import Button from '@guijun13/ui/src/components/Button';
 
 const Title = styled.h1`
@@ -8,10 +9,10 @@ const Title = styled.h1`
 
 export default function Home() {
   return (
-    <>
-      <Button />
+    <UIProvider>
+      <Button>Componente que veio da lib</Button>
       {/* <Button>Clique aqui</Button> */}
       <Title>Hello world!</Title>
-    </>
+    </UIProvider>
   );
 }
